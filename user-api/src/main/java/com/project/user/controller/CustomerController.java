600047f1a3c374f85fb5eac67f079e4c5c10b390
Jae-HyeokKim <jaehyeok.ethan@gmail.com>
2025-03-21 21:17:03 +0900

    feat: [006-CUSTOMER_balance] 커스터머에 대한 잔액 조회 구현.

--- user-api/src/main/java/com/project/user/controller/CustomerController.java
+++ user-api/src/main/java/com/project/user/controller/CustomerController.java
@@ -2,16 +2,15 @@ package com.project.user.controller;
 
 import com.project.domain.config.JwtAuthenticationProvider;
 import com.project.domain.domain.common.UserVo;
+import com.project.user.domain.customer.ChangeBalanceForm;
 import com.project.user.domain.customer.CustomerDto;
 import com.project.user.domain.model.Customer;
 import com.project.user.exception.CustomException;
 import com.project.user.service.CustomerService;
+import com.project.user.service.CustomerBalanceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.project.user.exception.ErrorCode.NOT_FOUND_USER;
 
@@ -22,6 +21,7 @@ public class CustomerController {
 
     private final JwtAuthenticationProvider provider;
     private final CustomerService customerService;
+    private final CustomerBalanceService customerBalanceService;
 
     @GetMapping("/getInfo")
     public ResponseEntity<CustomerDto> getInfo(@RequestHeader(name = "X-AUTH-TOKEN") String token) {
@@ -32,5 +32,11 @@ public class CustomerController {
         return ResponseEntity.ok(CustomerDto.from(c));
     }
 
+    @PostMapping("/balance")
+    public ResponseEntity<Integer> changeBalance(@RequestHeader(name = "X-AUTH-TOKEN") String token,
+                                                 @RequestBody ChangeBalanceForm form) {
+        UserVo vo = provider.getUserVo(token);
+        return ResponseEntity.ok(customerBalanceService.changeBalance(vo.getId(),form).getCurrentMoney());
+    }
 
 }
--- user-api/src/main/java/com/project/user/exception/ErrorCode.java
+++ user-api/src/main/java/com/project/user/exception/ErrorCode.java
@@ -13,6 +13,8 @@ public enum ErrorCode {
     EXPIRE_CODE(HttpStatus.BAD_REQUEST, "인증시간이 만료되었습니다."),
     WRONG_VERIFICATION(HttpStatus.BAD_REQUEST, "잘못된 인증 시도 입니다."),
 
+    NOT_ENOUGH_BALANCE(HttpStatus.BAD_REQUEST, "잔액이 부족합니다."),
+
 
     // login
     LOGIN_CHECK_FAIL(HttpStatus.BAD_REQUEST, "아이디나 패스워드를 확인해 주세요.");
